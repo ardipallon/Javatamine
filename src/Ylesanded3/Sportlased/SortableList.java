071a7edb4bb0f553f544b64082b2b21317612b32
ardip <ardi.pallon@khk.ee>
2019-01-30 09:13:45 +0200

    Paketid 1-5

---
+++ src/Ylesanded3/Sportlased/SortableList.java
@@ -0,0 +1,10 @@
+package Ylesanded3.Sportlased;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortableList<T extends Comparable<T>> extends ArrayList<T> {
+    public void sort() {
+        Collections.sort(this);
+    }
+}
